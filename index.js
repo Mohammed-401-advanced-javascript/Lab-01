e9252cb6251b410f0ef219181f5b2db53ee3fd58
MohammedAlhawamdeh <hawamdeh9393@gmail.com>
2020-01-23 17:54:56 +0200

    added the schema in index.js

--- index.js
+++ index.js
@@ -2,4 +2,30 @@
 
 const validator = require('./lib/validator.js');
 
-validator.isValid();
+// validator.isValid();
+
+
+// 
+validator.isValid = (schema, data) => {
+    let valid = true;
+    for (let fieldName in schema.fields) {
+        let field = schema.fields[fieldName];
+        // Am I required and set?
+        let required = field.required
+        ? validator.isTruthy(data[fieldName])
+        : true;
+        // Am I the right type (if we even care)
+        let type = field.type
+        ? validator.isCorrectType(data[fieldName], field)
+        : true;
+      // If anything is false ...
+      if (!(required && type)) {
+        valid = false;
+        console.log(data)
+    }
+}    
+
+
+return valid;
+};
+// console.log(validator.isArr([]))
